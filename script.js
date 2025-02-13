82d4f737a1acc80098786007577e37d84f772b40
Mishra Ravi Shivshankar <132126056+RAMRAMRAMR@users.noreply.github.com>
2025-02-13 12:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const val = document.getElementId("frame");
+
+input.addEventListener("blur", ()=>{
+	let val = input.value
+	input.value = val.toUpperCase();
+})
