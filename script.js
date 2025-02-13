3e8f130d6b4a9e8353ca0e0a766089f9cb79bbb4
Mishra Ravi Shivshankar <132126056+RAMRAMRAMR@users.noreply.github.com>
2025-02-13 12:55:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 
-const val = document.getElementId("frame");
+const val = document.getElementId("fname");
 
 input.addEventListener("blur", ()=>{
 	let val = input.value
